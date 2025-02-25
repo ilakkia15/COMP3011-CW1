5216a3d3f2a1be41254dd4f7ea3208db099eea01
Ilakkia Arunkumar <sc22ia@leeds.ac.uk>
2025-02-25 20:08:36 +0000

    Activate the admin site

--- rater/ratings/admin.py
+++ rater/ratings/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Professor, Module, Instance, Student, Rating
 
 # Register your models here.
+
+admin.site.register(Professor)
+admin.site.register(Module)
+admin.site.register(Instance)
+admin.site.register(Student)
+admin.site.register(Rating)
