08596f45617a361a2a16d50a3ffd903633ff4d44
Ilakkia Arunkumar <sc22ia@leeds.ac.uk>
2025-02-27 14:18:55 +0000

    Add API function to handle register request

--- rater/ratings/views.py
+++ rater/ratings/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Professor, Module, Instance, Student, Rating
 
 # Create your views here.
+
+def Register(request):
+    return HttpResponse('Handled Register Request')
