5117c73144e28e203e2fa667779150ae4a132c92
Ilakkia Arunkumar <sc22ia@leeds.ac.uk>
2025-03-01 17:18:07 +0000

    Rename Register function and override CSRF

--- rater/ratings/views.py
+++ rater/ratings/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Professor, Module, Instance, Student, Rating
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
-def Register(request):
+@csrf_exempt
+def HandleRegisterRequest(request):
     return HttpResponse('Handled Register Request')
