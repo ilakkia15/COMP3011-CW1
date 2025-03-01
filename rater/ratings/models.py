bd1f946b1816ced63cfd13cb29e5e9a9f331179b
Ilakkia Arunkumar <sc22ia@leeds.ac.uk>
2025-03-01 16:54:35 +0000

    Remove firstname and lastname from Student

--- rater/ratings/models.py
+++ rater/ratings/models.py
@@ -37,8 +37,6 @@ class Instance(models.Model):
         return self.professor + " - " + self.module
 
 class Student(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
     username = models.CharField(max_length=30, unique=True)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=128)
