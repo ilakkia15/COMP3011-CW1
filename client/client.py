92965d046be85df9bde938795ebe90b93d07911f
Ilakkia Arunkumar <sc22ia@leeds.ac.uk>
2025-03-02 10:56:51 +0000

    Add function to get command

--- client/client.py
+++ client/client.py
@@ -28,8 +28,50 @@ def Register():
     email = GetInput("email")
     password = GetInput("password")
 
-def main():
-    Register()
+def GetCommand():
+    # Create a boolean variable to determine whether the command is valid.
+    valid = False
+    # Loop until a valid command has been provided.
+    while not valid:
+        # Retrieve the command.
+        command = input("")
+        # Check whether the command is empty.
+        if len(command) == 0:
+            # Display a message to provide a valid command.
+            print("Please provide a valid command.")
+        else:
+            # Set the boolean variable to False to indicate non-empty user input.
+            empty = False
 
-if __name__=="__main__":
-    main()
+    return command.split()
+
+def RunClient():
+    # Run continuously.
+    while True:
+        # Get a command from the user.
+        command = GetCommand()
+
+        # TODO: Add calls to the corresponding functions.
+        # Check whether the command exists and the number of arguments is correct.
+        # Call the corresponding function if the command is valid.
+        if command == "register" and len(command) == 1:
+            Register()
+        elif command == "login" and len(command) == 2:
+            pass
+        elif command == "logout" and len(command) == 1:
+            pass
+        elif command == "list" and len(command) == 1:
+            pass
+        elif command == "view" and len(command) == 1:
+            pass
+        elif command == "average" and len(command) == 3:
+            pass
+        elif command == "rate" and len(command) == 6:
+            pass
+        # The command does not exist or the number of arguments is incorrect.
+        else:
+            # Display a message to provide a valid command.
+            print("Please provide a valid command.")
+
+if __name__=="__RunClient__":
+    RunClient()
