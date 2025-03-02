eeb103c9706dcd14cd2342519fd41d1c8edb8bd1
Ilakkia Arunkumar <sc22ia@leeds.ac.uk>
2025-03-02 10:25:40 +0000

    Add function to get user input

--- client/client.py
+++ client/client.py
@@ -3,48 +3,33 @@ import requests
 # Create a Session object as a global variable.
 s = requests.Session()
 
+def GetInput(input_field):
+    # Create a boolean variable to determine whether the user input is not empty.
+    empty = True
+    # Ask for the user input.
+    while empty:
+        # Retrieve the user input.
+        user_input = input(input_field.capitalize() + ": ")
+        # Check whether the user input is empty.
+        if len(user_input) == 0:
+            # Display a message to provide user input.
+            print("Please provide a " + input_field + ".")
+        else:
+            # Set the boolean variable to False to indicate non-empty user input.
+            empty = False
+
+    return user_input
+
 def Register():
     # Display a message to the user to provide the required details.
     print("Please provide a username, email and password to register.")
 
-    # Create a boolean variable to determine if the username is valid.
-    valid_username = False
-    # Ask for a valid username.
-    while not valid_username:
-        # Retrieve a username from the user.
-        username = input("Username: ")
-        # Check whether the username is empty.
-        if len(username) == 0:
-            # Display a message to provide a username.
-            print("Please provide a username.")
-        else:
-            # Set the boolean variable to True to indicate a valid username.
-            valid_username = True
+    username = GetInput("username")
+    email = GetInput("email")
+    password = GetInput("password")
 
-    # Create a boolean variable to determine if the email is valid.
-    valid_email = False
-    # Ask for a valid email.
-    while not valid_email:
-        # Retrieve a email from the user.
-        email = input("Email: ")
-        # Check whether the email is empty.
-        if len(email) == 0:
-            # Display a message to provide an email.
-            print("Please provide an email.")
-        else:
-            # Set the boolean variable to True to indicate a valid email.
-            valid_email = True
+def main():
+    Register()
 
-    # Create a boolean variable to determine if the password is valid.
-    valid_password = False
-    # Ask for a valid password.
-    while not valid_password:
-        # Retrieve a password from the user.
-        password = input("Password: ")
-        # Check whether the password is empty.
-        if len(password) == 0:
-            # Display a message to provide an password.
-            print("Please provide an password.")
-        else:
-            # Set the boolean variable to True to indicate a valid password.
-            valid_password = True
+if __name__=="__main__":
+    main()
